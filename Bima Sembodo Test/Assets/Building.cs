44198f5d22eeaa3d2c41ec8b982eb0f4a7d26ee3
unmorally2022 <unmorally2022@gmail.com>
2022-07-15 12:22:37 +0800

    First Commit

---
+++ Bima Sembodo Test/Assets/Building.cs
@@ -0,0 +1,173 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    public enum StateBuilding
+    {
+        Locked, CountingDown, LevelingUp
+    }
+
+    [SerializeField]
+    GameplayManager gameplayManager;
+    [SerializeField]
+    GameObject[] BuildingObject;
+
+    [SerializeField]
+    public string BuildingName;
+    [SerializeField]
+    TMPro.TMP_Text[] TMP_TimeLeft;
+
+    public TMPro.TMP_Text TMP_TimeLeftProperty;
+
+    private StateBuilding stateBuilding;
+
+    [SerializeField]
+    Building buildingNeedToUnlockThis;
+    [SerializeField]
+    int buildingLevelNeedToUnlockThis;
+
+    [SerializeField]
+    public int MaxLevel;
+    public int CurrentLevel;
+
+    [SerializeField]
+    public int[] SeccondNeedToUpgrade;    
+    public int currentSeccond;
+    [SerializeField]
+    int SpeedUpSeccond = 300;
+
+    IEnumerator Enum_TimerCountdown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (buildingNeedToUnlockThis != null) {
+            this.gameObject.SetActive(false);            
+        }
+        else
+        {
+            UnlockThisBuilding();
+        }
+
+        ReShowBuilding();
+    }
+
+    IEnumerator IE_TimerCountdown() {
+        while (true) {
+
+            if (CurrentLevel < MaxLevel)
+            {
+                TMP_TimeLeft[CurrentLevel - 1].text = "Time : " + SeccondToTime(currentSeccond);
+                if (TMP_TimeLeftProperty != null)
+                    TMP_TimeLeftProperty.text = "Time : " + SeccondToTime(currentSeccond);
+
+                switch (stateBuilding)
+                {
+                    case StateBuilding.CountingDown:
+                        currentSeccond -= 1;
+                        if (currentSeccond <= 0)
+                        {   
+                            stateBuilding = StateBuilding.LevelingUp;
+                            LevelUp();                           
+                        }
+                        break;
+                    case StateBuilding.LevelingUp:
+
+
+                        break;
+                }
+            }
+            else {
+                if(TMP_TimeLeftProperty!=null)
+                    TMP_TimeLeftProperty.text = "Max Level";
+                TMP_TimeLeft[CurrentLevel - 1].text = "Max Level";
+            }
+
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+
+    private void LevelUp() {
+        CurrentLevel += 1;
+        ReShowBuilding();
+        gameplayManager.ForceCheckBuildingToUnlock(this);
+        
+        currentSeccond = SeccondNeedToUpgrade[CurrentLevel - 1];
+        
+        stateBuilding = StateBuilding.CountingDown;
+    }
+
+    private void ReShowBuilding()
+    {
+        //Debug.Log(this.gameObject.name);
+        for (int i = 0; i < BuildingObject.Length; i++) {
+            if (i != CurrentLevel-1)
+            {
+                //Debug.Log(i);
+                BuildingObject[i].SetActive(false);
+            }
+        }
+        if (CurrentLevel < MaxLevel) { }
+        else
+        {
+            if (TMP_TimeLeftProperty != null)
+                TMP_TimeLeftProperty.text = "Max Level";
+            TMP_TimeLeft[CurrentLevel - 1].text = "Max Level";
+        }
+        BuildingObject[CurrentLevel-1].SetActive(true);
+        
+    }
+
+    private void UnlockThisBuilding() {        
+        CurrentLevel = 1;
+        currentSeccond = SeccondNeedToUpgrade[CurrentLevel - 1];
+
+        stateBuilding = StateBuilding.CountingDown;
+        gameObject.SetActive(true);
+
+        Enum_TimerCountdown = IE_TimerCountdown();
+        StartCoroutine(Enum_TimerCountdown);
+    }
+
+    public void ForceCheckBuildingToUnlock(Building _building) {
+        if (buildingNeedToUnlockThis != null) {
+            if (buildingNeedToUnlockThis.CurrentLevel >= buildingLevelNeedToUnlockThis && buildingNeedToUnlockThis == _building) {
+                UnlockThisBuilding();
+            }
+        }
+    }
+
+    string SeccondToTime(int seccond) {
+        System.TimeSpan time = System.TimeSpan.FromSeconds(seccond);
+
+        //here backslash is must to tell that colon is
+        //not the part of format, it just a character that we want in output
+        string str = time.ToString(@"hh\:mm\:ss");
+
+        return str;
+    }
+
+    public void SpeedUp() {
+        if (currentSeccond - SpeedUpSeccond <= 0)
+        {
+            currentSeccond = 0;
+        }
+        else {
+            SpeedUpSeccond -= SpeedUpSeccond;
+        }
+    }
+
+    public void InstantUpgrade() {
+        if (CurrentLevel < MaxLevel)
+        {
+            stateBuilding = StateBuilding.LevelingUp;
+            currentSeccond = 0;
+            LevelUp();
+            
+        }
+    }
+
+
+}
---
+++ Bima Sembodo Test/Assets/GameplayManager.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameplayManager : MonoBehaviour
+{
+    //TMPro.TMP_Text TMP_Time;
+    [SerializeField]
+    TMPro.TMP_Text TMP_Coin;
+    [SerializeField]
+    int coins = 0;
+    [SerializeField]
+    Building[] building;
+    Building SelectedBuilding;
+
+    [SerializeField]
+    GameObject PanelBuildingProperty, PanelMessage;
+    [SerializeField]
+    TMPro.TMP_Text TMP_TextMessage, TMP_TextBuildingName, TMP_TextTimeLeftProperty;
+    [SerializeField]
+    Button ButtonSpeedUp;
+
+    [SerializeField]
+    int SpeedUpAmmount = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //coins = 10;
+        updateCoinText();
+
+        PanelBuildingProperty.SetActive(false); PanelMessage.SetActive(false);
+    }
+
+    public void updateCoinText() {
+        TMP_Coin.text = "Coins : " + coins.ToString() + "\n" + "Speed Up : "+SpeedUpAmmount.ToString();
+    }
+
+    public void ForceCheckBuildingToUnlock(Building _building) {
+        for (int i = 0; i < building.Length; i++) {
+            building[i].ForceCheckBuildingToUnlock(_building);
+        }
+    }
+
+    public void ShowBuildingProperty() {
+        SetSpeedUpButton();
+        SelectedBuilding.TMP_TimeLeftProperty = TMP_TextTimeLeftProperty;
+        TMP_TextBuildingName.text = SelectedBuilding.BuildingName;
+        PanelBuildingProperty.SetActive(true);
+    }
+
+    private void SetSpeedUpButton() {
+        if (SpeedUpAmmount > 0)
+        {
+            ButtonSpeedUp.enabled = true;
+        }
+        else
+        {
+            ButtonSpeedUp.enabled = false;
+        }
+    }
+
+    public void GUI_5MinuteSpeedUp() {
+        if (SelectedBuilding != null)
+        {
+            if (SelectedBuilding.currentSeccond < SelectedBuilding.SeccondNeedToUpgrade[SelectedBuilding.CurrentLevel - 1])
+            {
+                SelectedBuilding.SpeedUp();
+                SpeedUpAmmount -= 1;
+                SetSpeedUpButton();
+            }
+            else {
+                TMP_TextMessage.text = "Max Level reached Mas E, jok ngeyel";
+                PanelMessage.SetActive(true);
+            }
+        }
+    }
+
+    public void GUI_InstantUpgrade() {
+        if (SelectedBuilding.CurrentLevel < SelectedBuilding.MaxLevel)
+        {
+            if (coins - 7 >= 0)
+            {
+                if (SelectedBuilding != null)
+                {
+                    SelectedBuilding.InstantUpgrade();
+                    coins -= 7;
+                    updateCoinText();
+                }
+            }
+            else
+            {
+                TMP_TextMessage.text = "Coin ga cukup Mas E";
+                PanelMessage.SetActive(true);
+            }
+        }
+        else {
+            TMP_TextMessage.text = "Max Level reached Mas E, jok ngeyel";
+            PanelMessage.SetActive(true);
+        }
+    }
+
+    public void GUI_CloseProperty()
+    {
+        if (SelectedBuilding != null)
+        {
+            SelectedBuilding.TMP_TimeLeftProperty = null;
+            SelectedBuilding = null;            
+        }
+
+        PanelBuildingProperty.SetActive(false);
+    }
+
+    public void GUI_CloseMessage()
+    {
+        if (SelectedBuilding != null)
+        {
+            PanelMessage.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+
+        
+#if UNITY_EDITOR
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit raycastHit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out raycastHit))
+            {
+                if (raycastHit.transform.tag == "BuildingObject") {
+                    GameObject _building = raycastHit.transform.parent.gameObject;
+                    if (_building != null) {
+                        SelectedBuilding = _building.GetComponent<Building>();
+                        ShowBuildingProperty();
+                    }
+                    //Debug.Log(_building.name);
+                }
+                //Transform objecthit = hit.transform;
+                
+                
+            }
+        }
+#else
+        if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            Ray raycast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit raycastHit;
+            if (Physics.Raycast(raycast, out raycastHit))
+            {
+                if (raycastHit.transform.tag == "BuildingObject") {
+                    GameObject _building = raycastHit.transform.parent.gameObject;
+                    if (_building != null) {
+                        SelectedBuilding = _building.GetComponent<Building>();
+                        ShowBuildingProperty();
+                    }
+                    //Debug.Log(_building.name);
+                }
+            }
+        }
+#endif
+    }
+}
